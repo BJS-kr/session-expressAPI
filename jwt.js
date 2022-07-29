be5c475cd10976f1281c4f08fedb6e7adb20a744
BJS-kr <hahajeng1234@gmail.com>
2022-07-30 02:28:14 +0900

    update

--- jwt.js
+++ jwt.js
@@ -13,7 +13,7 @@ function doubleForEach(arr_1, arr_2, fn) {
   }
 }
 
-function makeJWTStructure(jwt, name) {
+function makeJWTStructure(jwt, name = 'jwt') {
   return jwt.split('.').reduce((jwtObj, curr, i) => {
     jwtObj.structure[parts[i]] = curr;
     return jwtObj;
@@ -45,16 +45,21 @@ function compareEquality(firstJWT, secondJWT) {
 }
 
 function instruct(firstJWT, secondJWT) {
-  parts.reduce((instructions, key) => {
+  const possiblyEmptyInstructions = parts.reduce((instructions, key) => {
     const isSameLength = firstJWT.structure[key].length == secondJWT.structure[key].length;
     const isEqual = firstJWT.structure[key] == secondJWT.structure[key];
     isSameLength !== isEqual && instructions.push(`${RED}> ${key} lengths ${ isSameLength ? 'matches' : 'not matches'} but equality is ${isEqual} <`);
 
     return instructions;
-    }, []).forEach(instruction => console.log(instruction));
+  }, [])
+
+  const instructions = !possiblyEmptyInstructions.length ? [`${BLUE}> identically same <`] : possiblyEmptyInstructions;
+
+  instructions.forEach(instruction => console.log(instruction));
 }
 
 function showAndCompare(firstJWT, secondJWT) {
+  console.log(WHITE);
   console.log(`${firstJWT.name}:\n`, firstJWT.structure,'\n');
   console.log(`${secondJWT.name}:\n`, secondJWT.structure,'\n');
   compareLength(firstJWT, secondJWT);
@@ -65,11 +70,28 @@ function showAndCompare(firstJWT, secondJWT) {
 
 const jwt = require('jsonwebtoken');
 
-const withPayload = makeJWTStructure(jwt.sign({ session:'HangHae99_JWT' }, 'secret'), 'wPayload');
-const withoutPayload = makeJWTStructure(jwt.sign({}, 'secret'), 'woPayload');
+const withPayload = makeJWTStructure(jwt.sign({ session:'HangHae99_JWT' }, 'secret'), 'w_payload');
+const withoutPayload = makeJWTStructure(jwt.sign({}, 'secret'), 'wo_payload');
+
+const secret = makeJWTStructure(jwt.sign({}, 'secret'), 'secret');
+const anotherSecret = makeJWTStructure(jwt.sign({}, 'anotherLongerSecret'), 'another_secret');
+
+const same_1 = makeJWTStructure(jwt.sign({}, 'secret'), 'same1');
+const same_2 = makeJWTStructure(jwt.sign({}, 'secret'), 'same2');
+
+const sameExpiration_1 =  makeJWTStructure(jwt.sign({}, 'secret', { expiresIn: 10 }), 'same_exp_1');
+const sameExpiration_2 =  makeJWTStructure(jwt.sign({}, 'secret', { expiresIn: 10 }), 'same_exp_2');
+
+const expiration_1 =  makeJWTStructure(jwt.sign({}, 'secret', { expiresIn: 10 }), 'exp_1');
+const expiration_2 =  makeJWTStructure(jwt.sign({}, 'secret', { expiresIn: 100 }), 'exp_2');
+
+const algorithm_1 =  makeJWTStructure(jwt.sign({}, 'secret', { algorithm: 'HS256' }), 'alg_1');
+const algorithm_2 =  makeJWTStructure(jwt.sign({}, 'secret', { algorithm: 'HS512' }), 'alg_2');
+
 
 showAndCompare(withPayload, withoutPayload);
-
-
-
-
+showAndCompare(secret, anotherSecret);
+showAndCompare(same_1, same_2);
+showAndCompare(sameExpiration_1, sameExpiration_2);
+showAndCompare(expiration_1, expiration_2);
+showAndCompare(algorithm_1, algorithm_2);

269667d1e67a06d3e324eb5924f12e7b34c5f906
BJS-kr <hahajeng1234@gmail.com>
2022-07-29 21:55:42 +0900

    update

--- jwt.js
+++ jwt.js
@@ -1,44 +1,74 @@
-function makeJWTStructure(jwt) {
-  return jwt.split('.').reduce((jwtStructure, curr, i) => {
-    i === 0 && (jwtStructure.head = curr);
-    i === 1 && (jwtStructure.body = curr);
-    i === 2 && (jwtStructure.tail = curr);
+const YELLOW = "\x1b[33m";
+const BLUE= "\x1b[34m";
+const MAGENTA = "\x1b[35m";
+const WHITE = "\x1b[37m";
+const RED = "\x1b[31m";
 
-    return jwtStructure;
+function makeJWTStructure(jwt, name) {
+  return jwt.split('.').reduce((jwtObj, curr, i) => {
+    i === 0 && (jwtObj.structure.head = curr);
+    i === 1 && (jwtObj.structure.body = curr);
+    i === 2 && (jwtObj.structure.tail = curr);
+
+    return jwtObj;
   }, {
-    original: jwt
+    original: jwt,
+    name,
+    structure:{}
   })
 }
 
-function compareLength(firstJWT, secondJWT) {
-  const [FTT, FH, FB, FT] = [firstJWT.original.length, firstJWT.head.length, firstJWT.body.length, firstJWT.tail.length];
-  const [STT, SH, SB, ST] = [secondJWT.original.length, secondJWT.head.length, secondJWT.body.length, secondJWT.tail.length];
-  console.log('Total length comparison: ', FTT, FTT === STT ? '==' : '!=', STT);
-  console.log('Head length comparison: ', FH, FH === SH ? '==' : '!=', SH);
-  console.log('Body length comparison: ', FB, FB === SB ? '==' : '!=', SB);
-  console.log('Tail length comparison: ', FT, FT === ST ? '==' : '!=', ST);
+function generateCompareString(length1, length2) {
+ return `${WHITE + length1} ${length1 === length2 ? YELLOW + '==' : MAGENTA + '!='} ${WHITE + length2}`
 }
 
-/**
- * 1. jwt의 원리
- * 2. jwt를 변형했을 때?
- * 3. validate와 decode의 차이
- * 4. payload와 각종 옵션들의 의미
- * 5. jwt는 암호화?
- * 6. jwt에 해쉬알고리즘이 필요한 이유
- * 7. jwt의 용도
- * 8. jwt는 어디에 위치해야 하는가
- */
+function compareLength(firstJWT, secondJWT) {
+  const { structure:{ head:FH, body:FB, tail:FT } } = firstJWT;
+  const { structure:{ head:SH, body:SB, tail:ST } } = secondJWT;
+
+  const [FTTL, FHL, FBL, FTL] = [firstJWT.original.length, FH.length, FB.length, FT.length];
+  const [STTL, SHL, SBL, STL] = [secondJWT.original.length, SH.length, SB.length, ST.length];
+  console.log(BLUE,'Total length comparison: ', generateCompareString(FTTL, STTL));
+  console.log(BLUE, 'Head length comparison: ', generateCompareString(FHL, SHL));
+  console.log(BLUE, 'Body length comparison: ', generateCompareString(FBL, SBL));
+  console.log(BLUE, 'Tail length comparison: ', generateCompareString(FTL, STL));
+}
+
+function compareEquality(firstJWT, secondJWT) {
+  console.log(' Head equal: ', firstJWT.structure.head == secondJWT.structure.head);
+  console.log(' Body equal: ', firstJWT.structure.body == secondJWT.structure.body);
+  console.log(' Tail equal: ', firstJWT.structure.tail == secondJWT.structure.tail);
+}
+
+function instruct(firstJWT, secondJWT) {
+['head', 'body', 'tail'].reduce((instructions, key) => {
+  const isSameLength = firstJWT.structure[key].length == secondJWT.structure[key].length;
+  const isEqual = firstJWT.structure[key] == secondJWT.structure[key];
+  isSameLength !== isEqual && instructions.push(`${RED}> ${key} lengths ${ isSameLength ? 'matches' : 'not matches'} but equality is ${isEqual} <`);
+
+  return instructions
+  }, []).forEach(instruction => console.log(instruction))
+
+  
+}
+
+function showAndCompare(firstJWT, secondJWT) {
+  console.log(`${firstJWT.name}:\n`, firstJWT.structure,'\n');
+  console.log(`${secondJWT.name}:\n`, secondJWT.structure,'\n');
+  compareLength(firstJWT, secondJWT);
+
+  compareEquality(firstJWT, secondJWT);
+
+  instruct(firstJWT, secondJWT);
+}
+
 
 const jwt = require('jsonwebtoken');
-// jwt의 원리
-const withPayload = makeJWTStructure(jwt.sign({session:'HangHae99_JWT'}, 'firstSecret', { expiresIn: 0, algorithm: 'HS256' }));
-const withoutPayload = makeJWTStructure(jwt.sign({}, 'secondLongerSecret', { expiresIn: '10d', algorithm: 'HS512'}));
 
+const withPayload = makeJWTStructure(jwt.sign({session:'HangHae99_JWT'}, 'secret', ), 'wPayload');
+const withoutPayload = makeJWTStructure(jwt.sign({}, 'secret'), 'woPayload');
 
-console.log({ withPayload }, '\n');
-console.log({ withoutPayload }, '\n');
-compareLength(withPayload, withoutPayload);
+showAndCompare(withPayload, withoutPayload);
 
 
 
